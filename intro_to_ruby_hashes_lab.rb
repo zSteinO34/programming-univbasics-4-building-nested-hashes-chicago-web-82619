13ad9b2a149af2d999e979313d428305ae088822
Zac Steinberg <zstein1234@gmail.com>
2019-08-13 21:18:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,12 @@ def monopoly_with_third_tier
   monopoly = {
     :railroads => {
       :pieces => 4,
-      :rent_in_dollars => []
+      :rent_in_dollars => {
+        :one => 1,
+        :two => 2,
+        :three => 3,
+        :four => 4
+      }
     }
   }
 end
