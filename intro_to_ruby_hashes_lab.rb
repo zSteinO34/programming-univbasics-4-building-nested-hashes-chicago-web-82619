4a245a68aa1889cf2d84445599a96fb3fdfea27e
Zac Steinberg <zstein1234@gmail.com>
2019-08-13 21:39:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,10 +23,18 @@ def monopoly_with_third_tier
         :four_pieces_owned => 200
       },
       :names => {
-        :reading_railroad => {},
-        :pennsylvania_railroad => {},
-        :b_and_o_railroad => {},
-        :shortline_railroad => {}
+        :reading_railroad => {
+          "mortgage_value"=>"$100"
+        },
+        :pennsylvania_railroad => {
+          "mortgage_value"=>"$200"
+        },
+        :b_and_o_railroad => {
+          "mortgage_value"=>"$400"
+        },
+        :shortline_railroad => {
+          "mortgage_value"=>"$100"
+        }
       }
     }
   }
