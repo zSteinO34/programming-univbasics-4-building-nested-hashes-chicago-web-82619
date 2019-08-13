13f91038eebf8ac56c16816ddeb0d40eb00b5c62
Zac Steinberg <zstein1234@gmail.com>
2019-08-13 21:40:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -44,7 +44,7 @@ def monopoly_with_fourth_tier
       },
       :names => {
         :reading_railroad => {
-          :mortgage_value =>"$100"
+          "mortgage_value"=>"$100"
         },
         :pennsylvania_railroad => {
           "mortgage_value"=>"$200"
